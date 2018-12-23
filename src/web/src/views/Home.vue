36e2d821dc2db08c939ba38d0e95bd2077214fe7
modalone <840455798@qq.com>
2018-12-23 14:52:51 +0800

    初始化前端代码

---
+++ src/web/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <carousel></carousel>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import carousel from '@/components/carousel/index.vue'
+
+export default {
+  name: 'home',
+  components: {
+    carousel
+  },
+  data() {
+    return {
+     
+    };
+  },
+  methods: {
+    
+  }
+}
+</script>
